f517c6da3c5dd682712d9b9e9257b758af6578dd
wangfeng <wangfeng15@yfpharmacy.com>
2024-09-27 17:47:33 +0800

    feat: init

--- src/sidepanel/Sidepanel.vue
+++ src/sidepanel/Sidepanel.vue
@@ -1,22 +1,39 @@
 <script setup lang="ts">
 import { storageDemo } from '~/logic/storage'
 
+const currentUrl = ref<string | undefined>('')
+const currentParamObj = ref()
+function getUrlParams(url: string) {
+  const params = new URLSearchParams(url.split('?')[1])
+  const result: Record<string, string> = {}
+
+  for (const [key, value] of params.entries()) {
+    result[key] = value
+  }
+
+  return result
+}
+// _openx_header
+
+chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+  const currentTab = tabs[0]
+  currentUrl.value = currentTab.url
+
+  currentParamObj.value = getUrlParams(currentUrl.value as string)
+
+  console.log(currentParamObj.value, '-----')
+})
+
 function openOptionsPage() {
   browser.runtime.openOptionsPage()
 }
 </script>
-
 <template>
-  <main class="w-full px-4 py-5 text-center text-gray-700">
-    <Logo />
-    <div>Sidepanel</div>
-    <SharedSubtitle />
+  <main class="w-full text-center">
+    <div class="mt-2"><span class="opacity-50">Storage:</span> {{ storageDemo }}</div>
 
-    <button class="btn mt-2" @click="openOptionsPage">
-      Open Options
-    </button>
-    <div class="mt-2">
-      <span class="opacity-50">Storage:</span> {{ storageDemo }}
-    </div>
+    <!-- <textarea class="mt-2" type="text" v-model="currentUrl" /> -->
+
+    <button class="btn mt-2" @click="openOptionsPage">Open Options</button>
   </main>
 </template>
