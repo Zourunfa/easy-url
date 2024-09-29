10a41c4cfab147c85886004bcb9afd7999ddc826
Zourunfa <473023989@qq.com>
2024-09-29 14:43:17 +0800

    feat: json的展示和编辑,刷新和跳转新openxHeader的功能

--- src/sidepanel/Sidepanel.vue
+++ src/sidepanel/Sidepanel.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import { storageDemo } from '~/logic/storage'
-// import JsonEditor from 'vue-json-editor'
-// import 'vue-json-editor/dist/jsoneditor.min.css'
 import Base62x from '@pluve/base62'
-// import VueJsonEditor from 'vue3-json-viewer
-import JsonViewer from 'vue3-json-viewer'
+import JsonEditor from 'vue3-ts-jsoneditor'
+
 const currentUrl = ref<string | undefined>('')
 const currentParamObj = reactive<Record<string, string>>({})
 const currentOpenxHeader = ref('')
@@ -27,36 +25,55 @@ chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
 
   currentParamObj.value = getUrlParams(currentUrl.value as string)
 
-  console.log(currentParamObj.value, '-----')
-  // alert(currentParamObj.value._openx_header)
   currentOpenxHeader.value = currentParamObj.value._openx_header || ''
 
   currentOpenxHeaderObj.value = currentOpenxHeader.value ? JSON.parse(Base62x.decode(currentOpenxHeader.value)) : undefined
-  console.log(currentOpenxHeaderObj.value, '-----')
-  // alert(JSON.stringify(currentOpenxHeaderObj.value))
 })
 
-function openOptionsPage() {
-  browser.runtime.openOptionsPage()
-}
-function onJsonChange(value) {
-  console.log('value:', value)
-}
+const queryLanguages = ref(['javascript', 'lodash', 'jmespath'])
 
-function onModeChange(value) {
-  console.log('value:', value)
+const changeURLArg = (url: string, arg: string, arg_val: string) => {
+  var pattern = arg + '=([^&]*)'
+  var replaceText = arg + '=' + arg_val
+
+  if (url.match(pattern)) {
+    var tmp = '(' + arg + '=)([^&]*)'
+    return url.replace(new RegExp(tmp, 'gi'), replaceText)
+  } else {
+    alert(2)
+    if (url.match('[\?]')) {
+      return url + '&' + replaceText
+    } else {
+      return url + '?' + replaceText
+    }
+  }
 }
-const state = reactive({
-  json: {},
-})
+const handleJsonChange = content => {
+  currentOpenxHeader.value = Base62x.encode(JSON.stringify(currentOpenxHeaderObj.value))
+  currentUrl.value = changeURLArg(currentUrl.value as string, '_openx_header', currentOpenxHeader.value)
+}
+const handleSkipNewTab = () => {
+  chrome.tabs.create({ url: currentUrl.value })
+}
+const handleReplaceUrl = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    if (tabs.length > 0) {
+      const currentTab = tabs[0]
+      chrome.tabs.update(currentTab.id, { url: currentUrl.value })
+    }
+  })
+}
 </script>
 <template>
   <main class="w-full text-center">
     <div class="text-center w-90% p-4">
-      <textarea v-model="currentUrl" class="w-full h-24 resize-none p-2 border border-gray-300 rounded-md bg-white text-gray-700 focus:outline-none focus:border-blue-500"> </textarea>
+      <textarea v-model="currentUrl" class="w-full h-36 resize-none p-2 border border-gray-300 rounded-md bg-white text-gray-700 focus:outline-none focus:border-blue-500"> </textarea>
       <textarea v-model="currentOpenxHeader" class="w-full h-24 resize-none p-2 border border-gray-300 rounded-md bg-white text-gray-700 focus:outline-none focus:border-blue-500" />
-      <!-- <Vue3JsonEditor v-model="currentOpenxHeaderObj" :show-btns="true" :expandedOnStart="true" @json-change="onJsonChange" /> -->
-      <JsonViewer :value="currentOpenxHeaderObj" copyable boxed sort theme="light" @onKeyClick="keyClick" />
+      <json-editor height="400" mode="tree" :queryLanguagesIds="queryLanguages" @change="handleJsonChange" v-model:json="currentOpenxHeaderObj" />
+      <div class="flex justify-center gap-4">
+        <button class="btn mt-2" @click="handleReplaceUrl">刷新当前url</button>
+        <button class="btn mt-2" @click="handleSkipNewTab">用新url跳转新TAB</button>
+      </div>
     </div>
   </main>
 </template>
