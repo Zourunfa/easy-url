e65d2c8adfbad2c51d4eaa47e24913e877d68d69
Zourunfa <wangfeng15@yfpharmacy.com>
2024-09-30 17:25:01 +0800

    feat: Reproduction

--- src/sidepanel/Sidepanel.vue
+++ src/sidepanel/Sidepanel.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import { storageDemo } from '~/logic/storage'
 import Base62x from '@pluve/base62'
-import JsonEditor from 'vue3-ts-jsoneditor'
+import JsonEditorVue from 'json-editor-vue3'
 
 const currentUrl = ref<string | undefined>('')
 const currentParamObj = reactive<Record<string, string>>({})
 const currentOpenxHeader = ref('')
-const currentOpenxHeaderObj = ref()
+const currentOpenxHeaderObj = ref(undefined)
 
 function getUrlParams(url: string) {
   const params = new URLSearchParams(url.split('?')[1])
@@ -22,15 +21,10 @@ function getUrlParams(url: string) {
 chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
   const currentTab = tabs[0]
   currentUrl.value = currentTab.url
-
   currentParamObj.value = getUrlParams(currentUrl.value as string)
-
   currentOpenxHeader.value = currentParamObj.value._openx_header || ''
-
   currentOpenxHeaderObj.value = currentOpenxHeader.value ? JSON.parse(Base62x.decode(currentOpenxHeader.value)) : undefined
 })
-
-const queryLanguages = ref(['javascript', 'lodash', 'jmespath'])
 
 const changeURLArg = (url: string, arg: string, arg_val: string) => {
   var pattern = arg + '=([^&]*)'
@@ -48,7 +42,7 @@ const changeURLArg = (url: string, arg: string, arg_val: string) => {
     }
   }
 }
-const handleJsonChange = content => {
+const handleJsonChange = () => {
   currentOpenxHeader.value = Base62x.encode(JSON.stringify(currentOpenxHeaderObj.value))
   currentUrl.value = changeURLArg(currentUrl.value as string, '_openx_header', currentOpenxHeader.value)
 }
@@ -68,8 +62,7 @@ const handleReplaceUrl = () => {
   <main class="w-full text-center">
     <div class="text-center w-90% p-4">
       <textarea v-model="currentUrl" class="w-full h-36 resize-none p-2 border border-gray-300 rounded-md bg-white text-gray-700 focus:outline-none focus:border-blue-500"> </textarea>
-      <textarea v-model="currentOpenxHeader" class="w-full h-24 resize-none p-2 border border-gray-300 rounded-md bg-white text-gray-700 focus:outline-none focus:border-blue-500" />
-      <json-editor height="500" mode="tree" :queryLanguagesIds="queryLanguages" @change="handleJsonChange" v-model:json="currentOpenxHeaderObj" />
+      <json-editor-vue v-model="currentOpenxHeaderObj" style="height: 500px" class="editor" @change="handleJsonChange" current-mode="tree" />
       <div class="flex justify-center gap-4">
         <button class="btn mt-2" @click="handleReplaceUrl">刷新当前url</button>
         <button class="btn mt-2" @click="handleSkipNewTab">用新url跳转新TAB</button>
--- src/sidepanel/main.ts
+++ src/sidepanel/main.ts
@@ -1,18 +1,9 @@
 import { createApp } from 'vue'
+import App from './Sidepanel.vue'
 import { setupApp } from '~/logic/common-setup'
 import '../styles'
-import App from './Sidepanel.vue'
-import { JsonEditorPlugin } from 'vue3-ts-jsoneditor'
+
 const app = createApp(App)
 setupApp(app)
-app.use(JsonEditorPlugin, {
-  componentName: '/componentName/', // Default: 'JsonEditor',
-  options: {
-    /**
-     *
-     * SET GLOBAL OPTIONS
-     *
-     * */
-  },
-})
+
 app.mount('#app')
