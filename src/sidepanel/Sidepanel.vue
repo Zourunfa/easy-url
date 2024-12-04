7ee8008463e399c1b00a3a2034fbd866ac0d9b08
wangfeng <wangfeng15@yfpharmacy.com>
2024-12-04 15:06:40 +0800

    fix: my-json-editor

--- src/sidepanel/Sidepanel.vue
+++ src/sidepanel/Sidepanel.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import Base62x from '@pluve/base62'
-import JsonEditorVue from 'json-editor-vue3'
+// import JsonEditor from 'my-json-editor'
 
 const currentUrl = ref<string | undefined>('')
 const currentParamObj = reactive<Record<string, string>>({})
@@ -18,39 +18,46 @@ function getUrlParams(url: string) {
   return result
 }
 
-chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   const currentTab = tabs[0]
   currentUrl.value = currentTab.url
   currentParamObj.value = getUrlParams(currentUrl.value as string)
   currentOpenxHeader.value = currentParamObj.value._openx_header || ''
-  currentOpenxHeaderObj.value = currentOpenxHeader.value ? JSON.parse(Base62x.decode(currentOpenxHeader.value)) : undefined
+  currentOpenxHeaderObj.value = currentOpenxHeader.value
+    ? JSON.parse(Base62x.decode(currentOpenxHeader.value))
+    : undefined
 })
 
-const changeURLArg = (url: string, arg: string, arg_val: string) => {
-  var pattern = arg + '=([^&]*)'
-  var replaceText = arg + '=' + arg_val
+// function changeURLArg(url: string, arg: string, arg_val: string) {
+//   const pattern = `${arg}=([^&]*)`
+//   const replaceText = `${arg}=${arg_val}`
 
-  if (url.match(pattern)) {
-    var tmp = '(' + arg + '=)([^&]*)'
-    return url.replace(new RegExp(tmp, 'gi'), replaceText)
-  } else {
-    alert(2)
-    if (url.match('[\?]')) {
-      return url + '&' + replaceText
-    } else {
-      return url + '?' + replaceText
-    }
-  }
-}
-const handleJsonChange = () => {
-  currentOpenxHeader.value = Base62x.encode(JSON.stringify(currentOpenxHeaderObj.value))
-  currentUrl.value = changeURLArg(currentUrl.value as string, '_openx_header', currentOpenxHeader.value)
-}
-const handleSkipNewTab = () => {
+//   if (url.match(pattern)) {
+//     const tmp = `(${arg}=)([^&]*)`
+//     return url.replace(new RegExp(tmp, 'gi'), replaceText)
+//   }
+//   else {
+//     if (url.match('[\?]')) {
+//       return `${url}&${replaceText}`
+//     }
+//     else {
+//       return `${url}?${replaceText}`
+//     }
+//   }
+// }
+// function handleJsonChange() {
+//   currentOpenxHeader.value = Base62x.encode(JSON.stringify(currentOpenxHeaderObj.value))
+//   currentUrl.value = changeURLArg(
+//     currentUrl.value as string,
+//     '_openx_header',
+//     currentOpenxHeader.value,
+//   )
+// }
+function handleSkipNewTab() {
   chrome.tabs.create({ url: currentUrl.value })
 }
-const handleReplaceUrl = () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+function handleReplaceUrl() {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (tabs.length > 0) {
       const currentTab = tabs[0]
       chrome.tabs.update(currentTab.id, { url: currentUrl.value })
@@ -58,14 +65,31 @@ const handleReplaceUrl = () => {
   })
 }
 </script>
+
 <template>
   <main class="w-full text-center">
     <div class="text-center w-90% p-4">
-      <textarea v-model="currentUrl" class="w-full h-36 resize-none p-2 border border-gray-300 rounded-md bg-white text-gray-700 focus:outline-none focus:border-blue-500"> </textarea>
-      <json-editor-vue v-model="currentOpenxHeaderObj" style="height: 500px" class="editor" @change="handleJsonChange" current-mode="tree" />
+      <textarea
+        v-model="currentUrl"
+        class="w-full h-48 resize-none p-2 border border-gray-300 rounded-md bg-white text-gray-700 focus:outline-none focus:border-blue-500"
+      />
+      <!-- <textarea
+        v-model="currentOpenxHeader"
+        class="w-full h-36 resize-none p-2 border border-gray-300 rounded-md bg-white text-gray-700 focus:outline-none focus:border-blue-500"
+      /> -->
+
+      <!-- <JsonEditor
+        v-model="currentOpenxHeaderObj"
+        style="height: 700px"
+        @change="handleJsonChange"
+      /> -->
       <div class="flex justify-center gap-4">
-        <button class="btn mt-2" @click="handleReplaceUrl">刷新当前url</button>
-        <button class="btn mt-2" @click="handleSkipNewTab">用新url跳转新TAB</button>
+        <button class="btn mt-2" @click="handleReplaceUrl">
+          刷新当前url
+        </button>
+        <button class="btn mt-2" @click="handleSkipNewTab">
+          用新url跳转新TAB
+        </button>
       </div>
     </div>
   </main>
